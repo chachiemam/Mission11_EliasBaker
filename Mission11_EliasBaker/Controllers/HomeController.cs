240852ebfb5e64fe21617763f65e82f0f940613f
Eli Baker <ebaker42@byu.edu>
2024-03-18 20:13:01 -0600

    longer pages

--- Mission11_EliasBaker/Controllers/HomeController.cs
+++ Mission11_EliasBaker/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Mission11_EliasBaker.Controllers
 
         public IActionResult Index(int pageNum)
         {
-            int pageSize = 5;
+            int pageSize = 10;
             var blah = new BooksListViewModel
             {
                 Books = _bookRepo.Books
